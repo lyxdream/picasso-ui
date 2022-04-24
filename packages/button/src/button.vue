3478c394bb32fab232b1a537ae7125d098924b57
yinxia <liyinxia@weimiao.cn>
2022-04-24 19:34:24 +0800

    feat: button组件

--- packages/button/src/button.vue
+++ packages/button/src/button.vue
@@ -1,10 +1,43 @@
 <template>
-    <button>按钮</button>
+  <button>按钮</button>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue'
+import { defineComponent, PropType } from "vue";
+type IButtonType =
+  | "primary"
+  | "success"
+  | "warning"
+  | "danger"
+  | "info"
+  | "default";
 export default defineComponent({
+  props: {
+    type: {
+      type: String as PropType<IButtonType>,
+      default: "primary",
+      vaildator: (val: string) => {
+        return [
+          "default",
+          "primary",
+          "success",
+          "warning",
+          "danger",
+          "info",
+        ].includes(val);
+      },
+    },
+    icon: {
+      type: String,
+      default: "",
+    },
+    disabled: Boolean,
+    loading: Boolean,
+    round: Boolean,
+  },
   name: "PButton",
+  setup(props, ctx) {
+    props.type;
+  },
 });
 </script>
