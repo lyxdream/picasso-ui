67578c22cbcce6f2c81a1f465d81c859e796ad28
yinxia <liyinxia@weimiao.cn>
2022-05-07 19:01:18 +0800

    feat: message组件开发

---
+++ packages/message/index.ts
@@ -0,0 +1,10 @@
+import { App } from "vue";
+import Message from "./src/message";
+
+(Message as any).install = (app: App): void => {
+  //这个写法是为了兼容vue2 否则无法在this上使用
+  app.config.globalProperties.$message = Message;
+};
+//用户可以去引入组件 通过use的方式，或者可以直接导入
+export { Message };
+export default Message;
---
+++ packages/message/src/message.ts
@@ -0,0 +1,3 @@
+export default function Message() {
+  console.log("message---");
+}
